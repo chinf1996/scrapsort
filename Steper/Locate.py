5e7ac4d33fad30a86137b8c76dc5f789d8b23263
CHINF <chinf.work@gmail.com>
2018-10-12 11:39:19 +0800

    fix

--- Steper/Locate.py
+++ Steper/Locate.py
@@ -2,16 +2,15 @@ from steper import Steper
 
 class Locate:
     """The class can locate to a specified location"""
-    _steps = 0
     _lenStep = 1
     def __init__(self, lenStep=1):
         """Get the length of the stepper motor (mm)"""
-        _lenStep = lenStep
+        self._lenStep = lenStep
     
     
     def Go(self, distance):
         """Go to the specified distance, Let the stepper motor take (distance/lenStep) steps"""
-        _steps = distance/int(_lenStep)
+        _steps = distance/int(self._lenStep)
         Steper.NonlinearSpeed(int(_steps))
         
     
